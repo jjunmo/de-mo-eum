45c8d69847f78c012866494d2a92f45991ff6d44
jjunmo <yymeyy2002@gamil.com>
2022-04-30 13:54:50 +0900

    Initial commit

---
+++ src/main/java/kr/co/promisemomo/PromisemomoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.promisemomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromisemomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromisemomoApplication.class, args);
+	}
+
+}
