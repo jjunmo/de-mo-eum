a3443513d373f16e71f7719d3f4912a316cafbd3
semo <yymeyy2002@gamil.com>
2022-07-18 13:08:51 +0900

    PromiseRestController 작성중

--- src/main/java/kr/co/promisemomo/module/promise/controller/PromiseRestController.java
+++ src/main/java/kr/co/promisemomo/module/promise/controller/PromiseRestController.java
@@ -44,6 +44,40 @@ public class PromiseRestController {
         return ResponseEntity.ok().body(promiseService.getParticipatePromises(memberId));
     }
 
+    /**
+     * 
+     * @param memberId 로그인된 아이디
+     * @return 내가 주최한 약속정보
+     */
+    @GetMapping("/host")
+    public HttpEntity<Object> getMyHostPromises(@RequestParam("member_id") Long memberId){
+        Optional<Member> memberOptional = memberService.getMember(memberId);
+        if (memberOptional.isEmpty()) {
+            return ResponseEntity.badRequest().body("로그인이 필요합니다.");
+        }
+
+        return ResponseEntity.ok().body(promiseService.myHostPromiseList(memberId));
+    }
+
+
+    /**
+     * 
+     * @param memberId 로그인된 아이디
+     * @return 수락 대기중인 약속 리스트
+     * 
+     */
+    @GetMapping("/wait")
+    public HttpEntity<Object> waitPromise(@RequestParam("member_id") Long memberId){
+        Optional<Member> memberOptional = memberService.getMember(memberId);
+        if (memberOptional.isEmpty()) {
+            return ResponseEntity.badRequest().body("로그인이 필요합니다.");
+        }
+        return ResponseEntity.ok().body(promiseService.waitPromise(memberId));
+    }
+
+
+
+
 
 
     
