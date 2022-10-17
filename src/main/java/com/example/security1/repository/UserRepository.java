3f5c17141a7670759b078d9242756f49c8bc13c9
jjunmo <yymeyy2002@gmail.com>
2022-10-18 06:17:03 +0900

    Security 로그인 (defaultSuccessUrl error)

--- src/main/java/com/example/security1/repository/UserRepository.java
+++ src/main/java/com/example/security1/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
+    //select*from user where username = 1?
+    public User findByUsername(String username);
 
 }
