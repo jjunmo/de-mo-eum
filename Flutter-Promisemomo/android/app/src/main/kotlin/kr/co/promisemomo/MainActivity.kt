e7e7696afeb0c830198791f330c558c613c2aa92
jjunmo <jjunmo@jjunmo-2.local>
2024-08-21 15:10:29 +0900

    App,Client 분리

--- Flutter-Promisemomo/android/app/src/main/kotlin/kr/co/promisemomo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package kr.co.promisemomo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- NextJS-AppRouting-demo/app/test1/test3/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-  return (
-      <h2>test3</h2>
-  );
-}
